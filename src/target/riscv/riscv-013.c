508943e4c427a07099acff93d7864f6517d7b275
Aurore Poirier <aurore.poirier@scisemi.com>
2026-06-03 10:11:42 +0200

    riscv/cheriot: Read special capability registers
    Read special capability registers using the custom register addresses in
    the GDB register enum, starting at 0xC000.
    
    Co-authored-by: Owen Anderson <resistor@mac.com>
    Change-Id: I06c70b1c65d18ba6bdb3276eed783cfe2303a5d0

--- src/target/riscv/riscv-013.c
+++ src/target/riscv/riscv-013.c
@@ -935,6 +935,16 @@ static int register_read_abstract_with_size(struct target *target,
 	if (number >= GDB_REGNO_V0 && number <= GDB_REGNO_V31)
 		return ERROR_FAIL;
 
+    // Custom register numbers correspond to the special capability registers on CHERIOT
+    RISCV_INFO(rv_info)
+    if (number >= GDB_REGNO_COUNT && rv_info->cheriot) {
+        // Special capability registers are read by forcing a 64-bit CSR read using
+        // offset in the SCR address space.
+        number -= GDB_REGNO_COUNT;
+        number += GDB_REGNO_CSR0;
+        size = 64;
+    }
+
 	uint32_t command = riscv013_access_register_command(target, number, size,
 			AC_ACCESS_REGISTER_TRANSFER);
 	if (is_command_unsupported(target, command))
